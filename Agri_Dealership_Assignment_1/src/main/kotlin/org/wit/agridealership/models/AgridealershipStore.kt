a71d1e66124429ca30b8f7ec79d7dc46f6f5067a
John Hogan <46961201+20079707@users.noreply.github.com>
2020-11-06 20:01:10 +0000

    Update AgridealershipStore.kt

--- Agri_Dealership_Assignment_1/src/main/kotlin/org/wit/agridealership/models/AgridealershipStore.kt
+++ Agri_Dealership_Assignment_1/src/main/kotlin/org/wit/agridealership/models/AgridealershipStore.kt
@@ -3,6 +3,7 @@ package org.wit.agridealership.models
 interface AgridealershipStore {
     fun findAll(): List<AgridealershipModel>
     fun findOne(id: Long): AgridealershipModel?
+    fun findMake(Make: String): AgridealershipModel?
     fun create(agridealership: AgridealershipModel)
     fun update(agridealership: AgridealershipModel)
     fun delete(agridealership: AgridealershipModel)
